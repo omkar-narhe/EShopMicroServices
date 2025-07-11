e7defa17f1bbfa204b966bcf56c52c27cc066771
Omkar Narhe <Omkar.Narhe@agdata.com>
2025-07-11 15:33:08 +0530

    Added information for delet product endpoint

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -1,4 +1,6 @@
 ﻿
+using Catalog.API.Products.GetProductById;
+
 namespace Catalog.API.Products.DeleteProduct;
 
 //public record DeleteProductRequest(Guid id);
@@ -9,14 +11,18 @@ public class DeleteProductEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapDelete("/products/{id}",
-            async (Guid id, ISender sender) =>
-            {
-                var result = await sender.Send(new DeleteProductCommand(id));
+        app.MapDelete("/products/{id}",async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteProductCommand(id));
 
-                var response = result.Adapt<DeleteProductResponse>();
+            var response = result.Adapt<DeleteProductResponse>();
 
-                return Results.Ok(response);
-            });
+            return Results.Ok(response);
+        })
+        .WithName("DeleteProductByID")
+        .Produces<GetProductIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Delete Product by ID")
+        .WithDescription("Delete Product by ID");
     }
 }
