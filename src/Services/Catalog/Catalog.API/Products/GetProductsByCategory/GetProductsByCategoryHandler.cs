7e4f4f87922dde79a71ff0ce8db90c2cdfb5a4de
Omkar Narhe <Omkar.Narhe@agdata.com>
2025-07-11 14:40:02 +0530

    GetProductByCategory changes

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductsByCategoryHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Catalog.API.Products.GetProductsByCategory;
+
+public record GetProductsByCategoryRequest(string Category): IQuery<GetProductsByCategoryResult>;
+public record GetProductsByCategoryResult(IEnumerable<Product> Products);
+
+public class GetProductsByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductsByCategoryQueryHandler> logger) 
+    : IQueryHandler<GetProductsByCategoryRequest, GetProductsByCategoryResult>
+{
+    public async Task<GetProductsByCategoryResult> Handle(GetProductsByCategoryRequest query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductsByCategoryQueryHandler.Handle called with {@Query}", query);
+        var result = await session.Query<Product>()
+            .Where(x => x.Category.Contains(query.Category))
+            .ToListAsync(cancellationToken);
+
+        return new GetProductsByCategoryResult(result);
+        
+    }
+}
