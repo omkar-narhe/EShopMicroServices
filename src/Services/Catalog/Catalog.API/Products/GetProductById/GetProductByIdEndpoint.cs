a158e8e61a5f880c4fcc630ea0881bf004e80cbd
Omkar Narhe <Omkar.Narhe@agdata.com>
2025-07-11 13:47:26 +0530

    Get Product By Id changes

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,27 @@
+﻿
+using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.GetProductById;
+// public record GetProductIdRequest();
+
+public record GetProductIdResponse(Product Product);
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(id));
+
+            var response = result.Adapt<GetProductIdResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProductById")
+        .Produces<GetProductIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product by ID")
+        .WithDescription("Get Products by ID");
+    }
+}
