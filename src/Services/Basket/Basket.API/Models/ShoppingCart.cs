0eeaf6d335fef05e377c806b8b8c1c14ff1b6f22
Omkar Narhe <Omkar.Narhe@agdata.com>
2025-07-12 20:31:58 +0530

    Added domain classes

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿namespace Basket.API.Models;
+
+public class ShoppingCart
+{
+    public string Username { get; set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } = new();
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+    public ShoppingCart(string userName)
+    {
+        Username = userName;
+    }
+
+    //Required for mapping
+    public ShoppingCart()
+    {
+        
+    }
+}
