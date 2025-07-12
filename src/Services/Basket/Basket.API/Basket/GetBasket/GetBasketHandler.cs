2841ca782b39224c8552705f184cf2e252eb58ee
Omkar Narhe <Omkar.Narhe@agdata.com>
2025-07-12 21:15:24 +0530

    Get Basket end point

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,5 +1,16 @@
 ﻿namespace Basket.API.Basket.GetBasket;
 
-public class GetBasketHandler
+public record GetBasketQuery(string UserName): IQuery<GetBasketResult>;
+
+public record GetBasketResult(ShoppingCart Cart);
+public class GetBasketQueryHandler()
+    : IQueryHandler<GetBasketQuery, GetBasketResult>
 {
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        //TODO: Get basket from database
+        // var basket = await _repository.GetBasker(request.UserName)
+
+        return new GetBasketResult(new ShoppingCart("swn"));
+    }
 }
