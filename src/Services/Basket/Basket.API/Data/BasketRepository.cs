af024e1de87705de6266469960f660438f9d70d1
Omkar Narhe <Omkar.Narhe@agdata.com>
2025-07-15 20:19:29 +0530

    Initial changes for cached basked repository

--- src/Services/Basket/Basket.API/Data/BasketRepository.cs
+++ src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -2,13 +2,6 @@
 
 public class BasketRepository(IDocumentSession session) : IBasketRepository
 {
-    public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
-    {
-        session.Delete<ShoppingCart>(userName);
-        await session.SaveChangesAsync();
-        return true;
-    }
-
     public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
     {
         var basket = await session.LoadAsync<ShoppingCart>(userName, cancellationToken);
@@ -20,5 +13,12 @@ public class BasketRepository(IDocumentSession session) : IBasketRepository
         session.Store(basket);
         await session.SaveChangesAsync();
         return basket;
+    }
+
+    public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        session.Delete<ShoppingCart>(userName);
+        await session.SaveChangesAsync();
+        return true;
     }
 }
