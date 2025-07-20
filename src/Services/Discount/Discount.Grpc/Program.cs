bfdd96473484c03f59d74525c7338eb002c39213
Omkar Narhe <Omkar.Narhe@agdata.com>
2025-07-20 12:39:41 +0530

    Discount Service - Initial setup for migrations and db creation

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc();
 builder.Services.AddDbContext<DiscountContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DiscountConnection")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("Database")));
 
 var app = builder.Build();
 
