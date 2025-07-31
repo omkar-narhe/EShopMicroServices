1fd51b7fb99358ef667085f56c858517f351a3c1
Omkar Narhe <Omkar.Narhe@agdata.com>
2025-07-31 21:58:12 +0530

    Ordering Service: Rich Domain Models

--- src/Services/Ordering/Ordering.Domain/Models/Customer.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -4,4 +4,19 @@ public class Customer: Entity<CustomerId>
 {
     public string Name { get; private set; } = default!;
     public string Email { get; private set; } = default!;
+
+    public static Customer Create(CustomerId id, string name, string email)
+    {
+        ArgumentNullException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentNullException.ThrowIfNullOrWhiteSpace(email);
+
+        var Customer = new Customer
+        {
+            Id = id,
+            Name = name,
+            Email = email
+        };
+
+        return Customer;
+    }
 }
