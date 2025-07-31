8fc822f983983aa7d086f704101ac69eacd9d8a8
Omkar Narhe <Omkar.Narhe@agdata.com>
2025-07-31 21:05:05 +0530

    Ordering service: Domain models creation

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Domain.ValueObjects;
+
+public record Payment
+{
+    public string CardName { get;  } = default!;
+    public string CardNumber { get;  } = default!;
+    public string ExpirationDate { get;  } = default!;
+    public string CVV { get;  } = default!;
+    public decimal PaymentMethod { get;  } = default!;
+}
