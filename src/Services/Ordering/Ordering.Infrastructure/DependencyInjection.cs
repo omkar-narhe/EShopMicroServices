fad0504eeb9458bae6dd6b7b96edf69c3ef672f2
Omkar Narhe <Omkar.Narhe@agdata.com>
2025-08-03 13:13:18 +0530

    OS: Added Auditing Interceptors for IEntity

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -10,7 +10,11 @@ public static class DependencyInjection
         
         var connectionString = configuration.GetConnectionString("Database");
         services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(connectionString));
+        {
+            options.AddInterceptors(new AuditableEntityInterceptor());
+            options.UseSqlServer(connectionString);
+        });
+
         //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
         return services;
