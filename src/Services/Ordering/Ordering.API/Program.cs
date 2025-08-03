54d81e28be335d58e49ab85bb6f4e1aeda23bc2c
Omkar Narhe <Omkar.Narhe@agdata.com>
2025-08-03 11:59:41 +0530

    OS: Update Database commands automated using migrateAsync

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -3,6 +3,7 @@
 using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,5 +27,9 @@ builder.Services
 var app = builder.Build();
 
 // Configure HTTP Request pipeline
-
+app.UseApiServices();
+if(app.Environment.IsDevelopment())
+{
+    await app.InitializeDatabaseAsync();
+}
 app.Run();
---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure.Data.Extensions;
+
+public static class DatabaseExtensions
+{
+    public static async Task InitializeDatabaseAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        
+        dbContext.Database.MigrateAsync().GetAwaiter().GetResult();
+    }
+}
