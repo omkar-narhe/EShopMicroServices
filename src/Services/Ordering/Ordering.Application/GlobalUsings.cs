f93b94e3859368aef4db7df8b9c67432271ad5db
Omkar Narhe <Omkar.Narhe@agdata.com>
2025-08-04 22:13:51 +0530

    OS: Domain event handler created

--- src/Services/Ordering/Ordering.Application/GlobalUsings.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsings.cs
@@ -5,3 +5,6 @@ global using Ordering.Application.Data;
 global using Ordering.Domain.ValueObjects;
 global using FluentValidation;
 global using Ordering.Application.Exceptions;
+global using MediatR;
+global using Ordering.Domain.Events;
+global using Microsoft.Extensions.Logging;
