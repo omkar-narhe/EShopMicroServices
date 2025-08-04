c82973af3ceaa06b27b4b00595d8f887e98e6a6f
Omkar Narhe <Omkar.Narhe@agdata.com>
2025-08-04 22:43:33 +0530

    OS: Added GetOrdersByCustomer Query

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+public class GetOrdersByCustomerHandler(IApplicationDbContext dbContext)
+    : IQueryHandler<GetOrdersByCustomerQuery, GetOrdersByCustomerResult>
+{
+    public async Task<GetOrdersByCustomerResult> Handle(GetOrdersByCustomerQuery query, CancellationToken cancellationToken)
+    {
+        // Retrieve orders by customer ID from the database
+        var orders = await dbContext.Orders
+            .Include(o => o.OrderItems)
+            .AsNoTracking()
+            .Where(o => o.CustomerId == CustomerId.Of(query.CustomerId))
+            .OrderBy(o => o.OrderName.Value)
+            .ToListAsync(cancellationToken);
+
+        // Map the orders to the result DTO and return
+        return new GetOrdersByCustomerResult(orders.ToOrderDtoList());
+    }
+}
