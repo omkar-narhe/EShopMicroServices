a2182a248c47ec263830e7dacd203837022318d8
Omkar Narhe <Omkar.Narhe@agdata.com>
2025-08-04 21:52:44 +0530

    OS: Delete order command and handler added

--- src/Services/Ordering/Ordering.Application/GlobalUsings.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsings.cs
@@ -3,4 +3,5 @@ global using Ordering.Application.Dtos;
 global using Ordering.Domain.Models;
 global using Ordering.Application.Data;
 global using Ordering.Domain.ValueObjects;
+global using FluentValidation;
 global using Ordering.Application.Exceptions;
