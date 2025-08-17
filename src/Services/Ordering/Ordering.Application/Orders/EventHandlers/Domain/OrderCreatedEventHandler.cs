ce1d040bdb9299ffddf842f56740700d4614e5bb
Omkar Narhe <Omkar.Narhe@agdata.com>
2025-08-17 19:47:23 +0530

    Ordering Service: Added code for feature management for trigerring integration event

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,17 +1,27 @@
 ﻿using MassTransit;
+using Microsoft.FeatureManagement;
 
 namespace Ordering.Application.Orders.EventHandlers;
 
 public class OrderCreatedEventHandler
-    (IPublishEndpoint publishEndpoint, ILogger<OrderCreatedEventHandler> logger)
+    (IPublishEndpoint publishEndpoint, IFeatureManager featureManager, ILogger<OrderCreatedEventHandler> logger)
     : INotificationHandler<OrderCreatedEvent>
 {
     public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
     {
         logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
 
-        var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
-
-        await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+        // Publish the OrderCreatedIntegrationEvent to the message bus if feature flag is enabled.
+        if(await featureManager.IsEnabledAsync("OrderFullfilment"))
+        {
+            logger.LogInformation("Publishing OrderCreatedIntegrationEvent for Order ID: {OrderId}", domainEvent.order.Id);
+            var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+            await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+        }    
+        else
+        {
+            logger.LogInformation("Feature 'PublishOrderCreatedIntegrationEvent' is disabled. Skipping event publishing.");
+            return;
+        }
     }
 }
