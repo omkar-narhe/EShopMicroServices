ff607b8b11b632a152420bfbe3e129401f85c00f
Omkar Narhe <Omkar.Narhe@agdata.com>
2025-08-17 19:35:39 +0530

    Ordering Service: Added OrderCreatedEventHandler definition

--- src/Services/Ordering/Ordering.Application/Extensions/OrderExtensions.cs
+++ src/Services/Ordering/Ordering.Application/Extensions/OrderExtensions.cs
@@ -4,55 +4,6 @@ public static class OrderExtensions
 {
     public static IEnumerable<OrderDto> ToOrderDtoList(this IEnumerable<Order> orders)
     {
-        //List<OrderDto> orderDtos = new();
-        //foreach (var order in orders)
-        //{
-        //    var orderDto = new OrderDto
-        //    (
-        //        Id: order.Id.Value,
-        //        CustomerId: order.CustomerId.Value,
-        //        OrderName: order.OrderName.Value,
-        //        ShippingAddress: new AddressDto
-        //        (
-        //            FirstName: order.ShippingAddress.FirstName,
-        //            LastName: order.ShippingAddress.LastName,
-        //            EmailAddress: order.ShippingAddress.EmailAddress,
-        //            AddressLine: order.ShippingAddress.AddressLine,
-        //            Country: order.ShippingAddress.Country,
-        //            State: order.ShippingAddress.State,
-        //            ZipCode: order.ShippingAddress.ZipCode
-        //        ),
-        //        BillingAddress: new AddressDto
-        //        (
-        //            FirstName: order.BillingAddress.FirstName,
-        //            LastName: order.BillingAddress.LastName,
-        //            EmailAddress: order.BillingAddress.EmailAddress,
-        //            AddressLine: order.BillingAddress.AddressLine,
-        //            Country: order.BillingAddress.Country,
-        //            State: order.BillingAddress.State,
-        //            ZipCode: order.BillingAddress.ZipCode
-        //        ),
-        //        Payment: new PaymentDto
-        //        (
-        //            CardName: order.Payment.CardName,
-        //            CardNumber: order.Payment.CardNumber,
-        //            Expiration: order.Payment.ExpirationDate,
-        //            Cvv: order.Payment.CVV,
-        //            PaymentMethod: order.Payment.PaymentMethod
-        //        ),
-        //        Status: order.Status,
-        //        OrderItems: order.OrderItems.Select(item => new OrderItemDto
-        //        (
-        //            OrderId: item.OrderId.Value,
-        //            ProductId: item.ProductId.Value,
-        //            Quantity: item.Quantity,
-        //            Price: item.Price
-        //        )).ToList()
-        //    );
-
-        //    orderDtos.Add(orderDto);
-        //}
-        //return orderDtos;
         return orders.Select(order => new OrderDto(
            Id: order.Id.Value,
            CustomerId: order.CustomerId.Value,
